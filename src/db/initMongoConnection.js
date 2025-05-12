ecbb592b8a9e9a76d1177f89ab8079cf89268864
Roman <vrbox@i.ua>
2025-05-12 10:37:40 +0300

    Update InitDB

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -5,14 +5,14 @@ dotenv.config();
 
 //MONGODB_URI=mongodb+srv://dbAdmin:dbAdminPassword@contacts.42fklwl.mongodb.net/university?retryWrites=true&w=majority&appName=Contacts
 
-export function initMongoConnection() {
+export async function initMongoConnection() {
   const user = getEnvVar('MONGODB_USER');
   const pwd = getEnvVar('MONGODB_PASSWORD');
   const url = getEnvVar('MONGODB_URL');
   const db = getEnvVar('MONGODB_DB');
   const dbUrl = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`;
 
-  return mongoose
-    .connect(dbUrl)
-    .then(() => console.log('MongoDB connected...'));
+  await mongoose
+    .connect(dbUrl);
+  return console.log('MongoDB connected...');
 }
